c875b9098a008c27ba025ac8d957cf216674c1d6
sumedh <sumedh15@gmail.com.com>
2025-03-18 00:00:44 -0400

    added files

---
+++ store_index.py
@@ -0,0 +1,41 @@
+from src.helper import load_pdf_file,text_split,download_hugging_face_embeddings
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os 
+
+
+load_dotenv()
+
+import os
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
+
+extracted_data=load_pdf_file(data = 'Data/')
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "medicalbot"
+
+pc.create_index(
+    name=index_name,
+    dimension=384, # Replace with your model dimensions
+    metric="cosine", # Replace with your model metric
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    ) 
+)
+
+
+# embed each chuck and upsert the embedding into your Pinecone index
+from langchain_pinecone import PineconeVectorStore
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name = index_name,
+    embedding = embeddings
+)
